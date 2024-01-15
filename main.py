399cf9c03e8c4c76027496a0c847cf07a7df56f7
smichaux1 <smichaux@delawareav.com>
2024-01-15 08:49:07 -0500

    a description of your changes here

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 def main():
-    book_text = read_book("books/frankenstein.txt")
-    print(book_text)
+    book_path = "books/frankenstein.txt"
+    book_text = read_book(book_path)
+#    print(book_text)
     w_count = count_words(book_text)
-    print (f"word count is {w_count}")
+#    print (f"word count is {w_count}")
     l_count = count_letters(book_text)
-    print(l_count)
+#    print(l_count)
+    print_report(book_path,w_count,l_count)
     
 def read_book(book):  
     with open(book) as f:
@@ -16,11 +18,21 @@ def count_words(text):
 
 def count_letters(text):
     letter_count = {}
-    for letter in text:
-        if letter.lower() in letter_count:
-            letter_count[letter.lower()] += 1
-        else:
-            letter_count[letter.lower()] = 1
+    ltext = text.lower()
+    for letter in ltext:
+        if (letter in letter_count) and letter.isalpha():
+            letter_count[letter] += 1
+        elif letter.isalpha():
+            letter_count[letter] = 1
     return letter_count
+
+def print_report(path, count, dictionary):
+    letter_list = list(dictionary.items())
+    letter_list.sort(key=lambda x: x[1], reverse=True)
+    print(f"--- Begin report of {path} ---\n\n{count} words found in the document\n")
+ 
+    for x in range(0,len(letter_list)):
+        print(f"The '{letter_list[x][0]}' character was found {letter_list[x][1]} times")
+    print("--- End report ---")
 
 main()
